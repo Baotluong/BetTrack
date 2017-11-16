65c80e1eef3df54872f0d2320051bd55c7d15ae7
Bao Luong <baotluong@gmail.com>
2017-11-16 10:07:10 -0500

    Add project files.

---
+++ BetTrack/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BetTrack
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
